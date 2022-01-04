4280f1426fb36ef261a9b32ea913c43dce34878a
KostasA89 <konanast@gmail.com>
2022-01-04 15:26:01 +0200

    math

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,8 +1,19 @@
+def mathematics(n):
+    return lambda a: a*n
+
 def printList(l):
     for x in l:
         print(x)
 
+x = lambda a : a + 10
+y = lambda a,b: a*b
 
-l=["A","B","C"]
-printList(l)
+
+fmath = mathematics(2)
+
+print(fmath(5))
+
+#print(y(10,10))
+#l=["A","B","C"]
+#printList(l)
 
